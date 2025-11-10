79834c58e42b79e5738467814f49aacde3c5efc2
QuocKhai <khai10042005@gmail.com>
2025-11-10 20:32:06 +0700

    Sua khai

---
+++ SVGDemo_base/SVGDemo/Circle.h
@@ -0,0 +1,14 @@
+﻿#include "Common.h"
+#ifndef CIRCLE_H_
+#define CIRCLE_H_
+
+class Circle : public Element {
+public:
+    PointF center;
+    float radius;
+    void parseAttributes(xml_node<>* node) override;
+    void print();
+    void hello();
+    void ỉn
+};
+#endif
