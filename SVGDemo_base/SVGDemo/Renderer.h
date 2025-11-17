08d5d1ea14a730ea8f5101142b38129cf33c370f
QuocKhai <khai10042005@gmail.com>
2025-11-17 23:53:46 +0700

    Sua Circle Ellipse Stroke Color

--- SVGDemo_base/SVGDemo/Renderer.h
+++ SVGDemo_base/SVGDemo/Renderer.h
@@ -1,1 +1,25 @@
 #pragma once
+#include "stdafx.h"
+#include <vector>
+#include "Element.h" 
+#include "viewbox.h" 
+
+using std::vector;
+
+class Renderer {
+private:
+    vector<Element*> shapes;
+    viewbox mainViewbox;
+
+public:
+    Renderer();
+    ~Renderer();
+
+    void addShapes(const vector<Element*>& newshape);
+    void renAll(HDC hdc);
+    void pan(float dx, float dy);
+    void zoom(float factor);
+    void rotate(float angle);
+    viewbox& getViewbox();
+    void resetView();
+};
--- SVGDemo_base/SVGDemo/Stroke.h
+++ SVGDemo_base/SVGDemo/Stroke.h
@@ -1,11 +1,23 @@
-#include <iostream>
+#include "Color.h"
 #ifndef STROKE_H_
 #define	STROKE_H_
 
+
+
 class Stroke {
+private:
+	color strokeColor;
+	float strokeWidth;
 public:
-	float width;
-	// Color color;
+	Stroke();
+	~Stroke();
+
+	const color& getStrokeColor() const;
+	float getStrokeWidth() const;
+
+
+	void setStrokeColor(const color&);
+	void setStrokeWidth(float);
 };
 
 #endif
