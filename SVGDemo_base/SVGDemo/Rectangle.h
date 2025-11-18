ee21d860c62f7cf9cb5428e1cf6b9ac6891d4eb0
Âu Bảo Trân <aubaotran123@gmail.com>
2025-11-18 22:28:50 +0700

    Chinh sua lan cuoi milestone 1

--- SVGDemo_base/SVGDemo/Rectangle.h
+++ SVGDemo_base/SVGDemo/Rectangle.h
@@ -1,16 +1,24 @@
 #include "Element.h"
+#include "Point.h"
+
 
 #ifndef RECTANGLE_H_
 #define RECTANGLE_H_
 
-class Rectangle: public Element {
-public:
+class rectangle : public Element {
+private:
 	float width;
 	float height;
-	Point point;
-	Rectangle() : width(0), height(0), point(0,0) {}
-	~Rectangle() override = default;
+	point points;
+public:
+
+	rectangle();
+
+	~rectangle() override = default;
+
 	void parseAttributes(xml_node<>*) override;
+
+	void Draw(Gdiplus::Graphics* graphics) override;
 };
 
 #endif
--- SVGDemo_base/SVGDemo/Text.h
+++ SVGDemo_base/SVGDemo/Text.h
@@ -1,16 +1,28 @@
+#include "Point.h"
 #include "Element.h"
 
 #ifndef TEXT_H_
 #define TEXT_H_
 
 class Text: public Element {
-public:
-	Point position;
+private:
+	point position;
 	string content;
 	float fontsize;
-	Text() : position(0, 0), content(""), fontsize(0) {}
+	string font;
+	float dx;
+	float dy;
+	string textAnchor;
+	string fontStyle;
+	string fontWeight;
+public:
+	
+	Text() : position(0, 0), content(""), fontsize(16), font("Times New Roman"), dx(0), dy(0), textAnchor("start"), fontStyle("normal"), fontWeight("normal") {}
 	~Text() override = default;
+
 	void parseAttributes(xml_node<>*) override;
+
+	void Draw(Gdiplus::Graphics* graphics) override;
 };
 
 #endif
